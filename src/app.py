1651feda54eb3ac957b2d3f1928b20ffb96a5c65
Matthew Bowman <matthew.bowman@Sebastian.attlocal.net>
2024-04-23 19:24:40 -0500

    Prepare for GCP testing

---
+++ src/app.py
@@ -0,0 +1,12 @@
+# src/app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# Function to serve as entry point for GCF
+def main(request):
+    return app(request)
